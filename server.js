e05e5aab8a82602bcd92cac73f9d2d5639ca9ae4
rodgersea <elliottarodgers@gmail.com>
2020-05-23 15:33:31 -0400

    changed port for heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var path = require("path");
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
