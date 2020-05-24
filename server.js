978e19dab4b7a31c06390a0f545f38a2f3edb9af
rodgersea <elliottarodgers@gmail.com>
2020-05-23 20:00:12 -0400

    one missing capital letter :/

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"))
 
 require('./routes/apiRoutes')(app);
-require('./routes/htmlroutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, function() {
     console.log(`Successfully initialized server on PORT: ${PORT}`);
